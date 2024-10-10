b067dcfa28b655f6fddd8aa9a7e6af0c965f6ba6
Sevastian Cañari <sevastianprime@outlook.com>
2024-10-10 15:08:35 -0500

    feat: update port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const port = process.env.PORT || 3000
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
@@ -15,6 +15,6 @@ async function bootstrap() {
   );
 
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
