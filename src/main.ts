41b85653080bed0ffa3f407999b012fe5db41f8a
Sevastian58 <111455344+Sevastian58@users.noreply.github.com>
2024-10-12 07:33:46 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,6 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    })
-  );
-
-
   await app.listen(port);
 }
 bootstrap();
